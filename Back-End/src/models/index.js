f52ecf6485b1064f6901a64eed0a502b395bea03
Siphelele Miya <siphelelekunene2001@gmail.com>
2025-09-22 23:26:04 +0200

    removing back end

--- Back-End/src/models/index.js
+++
@@ -1,9 +0,0 @@
-import sequelize from "../dbconfig.js";
-import "./attachments.js";
-import "./infrastructure.js";
-import "./incident.js";
-import "./municipality.js";
-import "./sensorsModel.js";
-import "./userModel.js";
-
-export default sequelize;
