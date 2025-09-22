d6751fddd6ce89b87456c3a4306286b538cf40e3
Lungile <lungilethobejane8@gmail.com>
2025-09-22 09:37:36 +0200

    database setup

---
+++ Back-End/src/models/index.js
@@ -0,0 +1,9 @@
+import sequelize from "../dbconfig.js";
+import "./attachments.js";
+import "./infrastructure.js";
+import "./incident.js";
+import "./municipality.js";
+import "./sensorsModel.js";
+import "./userModel.js";
+
+export default sequelize;
